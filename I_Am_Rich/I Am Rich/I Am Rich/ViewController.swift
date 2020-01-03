cb469ec20e182c1f922f503210edf3e789424397
Betty <betty@24-124-93-58-dynamic.midco.net>
2020-01-02 20:46:13 -0600

    I am rich

---
+++ I_Am_Rich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Betty on 1/2/20.
+//  Copyright © 2020 Betty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
